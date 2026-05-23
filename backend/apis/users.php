b49b8b02a7e9798c87cea72220c86fec55476c15
akshatbhatti <akshat.netweb@gmail.com>
2026-05-23 09:45:55 +0000

    fixed cors errror

--- backend/apis/users.php
+++ backend/apis/users.php
@@ -2,7 +2,9 @@
 
 require_once __DIR__ . '/../db.php';
 
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 $method = $_SERVER['REQUEST_METHOD'];
 
